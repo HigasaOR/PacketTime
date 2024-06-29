1af10dd01f070c6fe8999d08e05e25b5fb82b8fb
HigasaOR <lee1003094395@gmail.com>
2024-06-29 10:13:58 +0800

    Initial Commit

---
+++ PacketTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PacketTime
+//
+//  Created by Chien Lee on 2024/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PacketTime/PacketTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacketTimeApp.swift
+//  PacketTime
+//
+//  Created by Chien Lee on 2024/6/29.
+//
+
+import SwiftUI
+
+@main
+struct PacketTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
